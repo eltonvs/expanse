3945bea6f6c859f2207efb53c37370b9a3a17d29
Elton Viana <eltonvs@outlook.com>
2017-03-22 13:20:49 -0300

    Removed Django project

--- README.txt
+++
@@ -1,9 +0,0 @@
-# How to run the project
-
-* Install python
-* Install pip
-* Install virtualenv
-* Create virtualenv
-
-- pip install requirements.txt
-- python manage.py runserver
--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(User)
-admin.site.register(Team)
-admin.site.register(Tournament)
-admin.site.register(Match)
