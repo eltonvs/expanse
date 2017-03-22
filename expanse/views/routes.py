da5fa15f3cda7640fffda652d5342a73f4108fde
yurialessandro <yuri.alessandro@hotmail.com>
2017-03-22 15:26:15 -0300

    views with oo

--- expanse/views/routes.py
+++ expanse/views/routes.py
@@ -1,6 +1,6 @@
 """
   Define the project routes
-  config.add_rout('[route_name]', 'url_path')
+  config.add_route('[route_name]', 'url_path')
     url_path --> expanse.com/url_path
 
   Created in 22/03/2017
@@ -9,5 +9,6 @@
 def includeme(config):
     # Routes:
     config.add_route('index', '/')
+    config.add_route('about', '/about')
 
     config.scan()
--- expanse/views/views.py
+++ expanse/views/views.py
@@ -1,8 +1,18 @@
-from pyramid.view import view_config
+from pyramid.view import view_config, view_defaults
 
-@view_config(route_name='index', renderer='templates/mytemplate.jinja2')
-def index(request):
-    # Do something
+@view_defaults( )
+class Expanse:
+    def __init__(self, request):
+        self.request = request
 
-    # Return aways a dict:
-    return {'title': 'expanse'}
+    @view_config(route_name='index', renderer='templates/mytemplate.jinja2')
+    def index(request):
+        # Do something
+
+        # Return aways a dict:
+        return {'title': 'expanse'}
+
+    @view_config(route_name='about', renderer='json')
+    def about(request):
+        a = 15
+        return {'value': a}
