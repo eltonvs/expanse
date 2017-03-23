dd8c1157023a4ab0d045a2723078269b42ff0c93
Elton Viana <eltonvs@outlook.com>
2017-03-23 00:09:24 -0300

    Add tests structure and models folder

---
+++ expanse/tests/test_views.py
@@ -0,0 +1,18 @@
+import unittest
+
+from pyramid import testing
+
+class ViewTests(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_index(self):
+        from ..views.views import ExpanseViews
+
+        request = testing.DummyRequest()
+        inst = ExpanseViews(request)
+        res = inst.index()
+        self.assertEqual('expanse', res['title'])
--- expanse/views/views.py
+++ expanse/views/views.py
@@ -1,9 +1,10 @@
 from pyramid.view import view_config, view_defaults
 
-@view_defaults( )
-class Expanse:
+@view_defaults(route_name='site')
+class ExpanseViews(object):
     def __init__(self, request):
         self.request = request
+        self.view_name = 'ExpanseViews'
 
     @view_config(route_name='index', renderer='templates/mytemplate.jinja2')
     def index(request):
