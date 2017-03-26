9809212aa5d717184c226b5cc2187daa82446eef
Elton Viana <eltonvs@outlook.com>
2017-03-26 16:44:37 -0300

    Create Login/Logout and add password encryption

--- expanse/__init__.py
+++ expanse/__init__.py
@@ -1,3 +1,5 @@
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.config import Configurator
 
 
@@ -5,6 +7,14 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+
+    # Security Policies
+    authn_policy = AuthTktAuthenticationPolicy(
+        settings['expanse.secret'],
+        hashalg='sha512')
+    authz_policy = ACLAuthorizationPolicy()
+    config.set_authentication_policy(authn_policy)
+    config.set_authorization_policy(authz_policy)
 
     # Jinja Settings
     config.include('pyramid_jinja2')
