4bb85ba116c0cda42a735ee64424a14cf175b645
Elton Viana <eltonvs@outlook.com>
2017-03-26 12:53:00 -0300

    Update tests and .travis.yml configs (and remove mongo configurations from __init__.py)

--- expanse/__init__.py
+++ expanse/__init__.py
@@ -1,15 +1,5 @@
 from pyramid.config import Configurator
 
-try:
-    # for python 2
-    from urlparse import urlparse
-except ImportError:
-    # for python 3
-    from urllib.parse import urlparse
-
-from gridfs import GridFS
-from pymongo import MongoClient
-
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -24,25 +14,6 @@ def main(global_config, **settings):
     config.include('.routes.routes')
 
     config.add_static_view('static', 'static', cache_max_age=3600)
-
-    # Database
-    db_url = urlparse(settings['mongo_uri'])
-    config.registry.db = MongoClient(
-        host=db_url.hostname,
-        port=db_url.port,
-    )
-
-    def add_db(request):
-        db = config.registry.db[db_url.path[1:]]
-        if db_url.username and db_url.password:
-            db.authenticate(db_url.username, db_url.password)
-        return db
-
-    def add_fs(request):
-        return GridFS(request.db)
-
-    config.add_request_method(add_db, 'db', reify=True)
-    config.add_request_method(add_fs, 'fs', reify=True)
 
     config.scan()
     return config.make_wsgi_app()
