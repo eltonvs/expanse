9f83dbfacdf91500d936b8b3a89e731b067d055a
yurialessandro <yuri.alessandro@hotmail.com>
2017-03-27 22:11:59 -0300

    add another layers for tournament

--- expanse/controllers/tournament.py
+++ expanse/controllers/tournament.py
@@ -1,1 +1,28 @@
-# controller
+from ..dao.tournament import TournamentDAO
+
+class TournamentController(object):
+    """Controller layer for Tournament object"""
+
+    def __init__(self):
+        self.tournament_dao = TournamentDAO()
+
+    def register(self, tournament):
+        err = self.validade(tournament)
+        if not err:
+            err = self.tournament_dao.insert(tournament)
+        return err
+
+    def validade(self, tournament):
+        err = {}
+        if not tournament.name:
+            err['empty_name'] = True
+
+        return err
+
+    def addTeam(self, tournamet, team):
+        if tournamet and team:
+            teams = tournamet.teams
+            teams.append(team)
+            tournamet.teams = teams
+
+
--- expanse/dao/tournament.py
+++ expanse/dao/tournament.py
@@ -1,1 +1,40 @@
-# dao
+from ..models.database import MongoDatabase
+from .generic import GenericDAO
+
+class TournamentDAO(GenericDAO):
+    __shared_state = {}
+
+    def __init__(self):
+        self.__dict__ = self.__shared_state
+        self.db = MongoDatabase().instance()
+
+    def insert(self, tournamet):
+        tournamet_to_insert = {
+            "name": tournamet.name,
+            "teams": [],
+            #"tournament_organizer_id": tournament_organizer
+        }
+        self.db.tournamets.insert(tournamet_to_insert)
+
+    def remove(self, tournamet):
+        print("Not implemented yet")
+        pass
+
+    def update(self, tournamet):
+        print("Not implemented yet")
+        pass
+
+    def add_team(self, team, tournamet):
+        # add new team to a tournamet
+        # need to find the _id of the current tournament
+        # insert team_id in the list of teams in db
+        print("Not implented yet")
+        pass
+
+    def get(self, tournamet):
+        print("Not implemented yet")
+        pass
+
+     def list(self):
+        tournamets = list(self.db.tournamets.find())
+        return tournamets
